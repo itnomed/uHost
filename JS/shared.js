86a6e1dcc1d11161af7fafa61fae5c191995fa8f
Aibek Omorov <bashatgroup777@gmail.com>
2021-03-23 19:32:49 +0600

    initial

---
+++ JS/shared.js
@@ -0,0 +1,17 @@
+
+// document.body.style.background= 'red'
+// setTimeout ( ()=> document.body.style.background='',3000);
+
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+
+for(var i = 0; i < selectPlanButtons.length; i++) {
+   selectPlanButtons[i].addEventListener('click', function () {
+      modal.style.display = 'block';
+      backdrop.style.display = 'block';
+   })
+}
+
+// console.dir(backdrop);
+// console.dir()
